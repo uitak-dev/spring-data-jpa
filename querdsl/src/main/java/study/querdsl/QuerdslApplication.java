26bcbfb770b72e77c7fc92d8be0fc7ca3f454f2c
정의탁 <justicexkr23@gmail.com>
2024-10-30 23:38:24 +0900

    querydsl init

---
+++ querdsl/src/main/java/study/querdsl/QuerdslApplication.java
@@ -0,0 +1,13 @@
+package study.querdsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerdslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerdslApplication.class, args);
+	}
+
+}
---
+++ querdsl/src/test/java/study/querdsl/QuerdslApplicationTests.java
@@ -0,0 +1,13 @@
+package study.querdsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerdslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
