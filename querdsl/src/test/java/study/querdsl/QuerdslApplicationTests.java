d4c03a5bd34e57b19cd171ca41b36be31555e394
정의탁 <justicexkr23@gmail.com>
2024-11-02 02:34:40 +0900

    update

--- querdsl/src/test/java/study/querdsl/QuerdslApplicationTests.java
+++ querdsl/src/test/java/study/querdsl/QuerdslApplicationTests.java
@@ -1,10 +1,16 @@
 package study.querdsl;
 
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class QuerdslApplicationTests {
+
+	@Autowired EntityManager em;
 
 	@Test
 	void contextLoads() {
